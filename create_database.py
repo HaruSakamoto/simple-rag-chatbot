61aa3ac6c2a4de1ee61a8351defb5273f3c9554a
HaruSakamoto <harusakamoto803@gmail.com>
2025-05-06 02:25:10 -0700

    Modified file

--- create_database.py
+++ create_database.py
@@ -60,7 +60,7 @@ def save_to_chroma(chunks: list[Document]):
     if os.path.exists(CHROMA_PATH):
         shutil.rmtree(CHROMA_PATH)
 
-    db = Chroma.from_documents(chunks,embedding,persist_directory=CHROMA_PATH)
+    db = Chroma.from_documents(chunks, embedding, persist_directory=CHROMA_PATH)
 
     db.persist()
     print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}")
